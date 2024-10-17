f2f059b201d42c1f7a2781ce4ec457e284a52a32
leomoraesitu <leomoraesitu@hotmail.com>
2024-10-17 20:07:35 -0300

    Implement feature: models

---
+++ lib/models/book.dart
@@ -0,0 +1,23 @@
+// lib/models/book.dart
+class Book {
+  final String id;
+  final String title;
+  final String coverUrl;
+  final String downloadUrl;
+
+  Book({
+    required this.id,
+    required this.title,
+    required this.coverUrl,
+    required this.downloadUrl,
+  });
+
+  factory Book.fromJson(Map<String, dynamic> json) {
+    return Book(
+      id: json['id'],
+      title: json['title'],
+      coverUrl: json['cover_url'],
+      downloadUrl: json['download_url'],
+    );
+  }
+}
